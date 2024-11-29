4c1ed8ef027f3da10051ae263b404ffe79a4e0be
Aniruddh Chaturvedi <Aniruddhchaturvedi2020@outlook.com>
2024-11-30 00:16:32 +0530

    Commit

--- src/main/java/org/example/jfxproject/Main.java
+++ src/main/java/org/example/jfxproject/Main.java
@@ -5,7 +5,11 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Line;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -14,19 +18,47 @@ public class Main extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        Group root = new Group();
-        Scene scene = new Scene(root, Color.BLACK);
+        Group root = new Group();       // root node
+        int width = 420, height = 420;
+        Scene scene = new Scene(root, width,height,Color.LIGHTSKYBLUE);       //width, height and color can be set seperately as well
 
         stage.setTitle("Epic and Peak Title");
         Image icon = new Image("car.png");
         stage.getIcons().add(icon);
 
-        stage.setWidth(420);
-        stage.setHeight(420);   // change
-        stage.setResizable(false);
+        //stage.setResizable(false);
+        Text txt = new Text("Testing text");
 
-        stage.setX(50);
-        stage.setY(50);
+        txt.setX(width/2);
+        txt.setY(height/2);
+        txt.setFont(Font.font(20));
+        txt.relocate(width - txt.getBoundsInParent().getCenterX(),height/2);
+        txt.setFill(Color.LIMEGREEN);
+
+
+        // random triangle generation
+
+        double x1 = (Math.random() * 420) % 420;
+        double x2 = (Math.random() * 420) % 420;
+        double x3 = (Math.random() * 420) % 420;
+        double y1 = (Math.random() * 420) % 420;
+        double y2 = (Math.random() * 420) % 420;
+        double y3 = (Math.random() * 420) % 420;
+
+        Line v1 = new Line();
+        v1.setStartX(x1); v1.setStartY(y1); v1.setEndX(x2); v1.setEndY(y2);
+
+        Line v2 = new Line();
+        v2.setStartX(x2); v2.setStartY(y2); v2.setEndX(x3); v2.setEndY(y3);
+
+        Line v3 = new Line();
+        v3.setStartX(x3); v3.setStartY(y3); v3.setEndX(x1); v3.setEndY(y1);
+
+        var nodes = root.getChildren();
+        nodes.add(v1);
+        nodes.add(v2);
+        nodes.add(v3);
+        nodes.add(txt);
 
         stage.setScene(scene);
         stage.show();
