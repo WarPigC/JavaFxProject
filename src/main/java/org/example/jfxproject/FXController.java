b695e85b0030ea876378b3414036375899ef9044
WarPigC <callaniruddhji@gmail.com>
2024-12-11 04:52:32 +0530

    removed redundant code

--- src/main/java/org/example/jfxproject/FXController.java
+++ src/main/java/org/example/jfxproject/FXController.java
@@ -47,27 +47,6 @@ public class FXController{
 
         triangle.setLayoutX(triangle.getLayoutX() + (Math.random() * 50) * ((Math.random() < 0.5) ? -1 : 1));
         triangle.setLayoutY(triangle.getLayoutY() + (Math.random() * 50) * ((Math.random() < 0.5) ? -1 : 1));
-
-
-
-        // TODO: going out of bounds??
-
-        morph.setLayoutX(morph.getLayoutX() + (Math.random() * 100) * ((Math.random() <= 0.5) ? -1 : 1));
-        morph.setLayoutY(morph.getLayoutY() + (Math.random() * 100) * ((Math.random() <= 0.5) ? -1 : 1));
-
-        if (triangle.getScaleX() > anchor.getScaleX()){
-            triangle.setScaleX(Math.abs(triangle.getScaleX() - anchor.getScaleX()));
-        }
-
-        if (triangle.getScaleY() > anchor.getScaleX()){
-            triangle.setScaleY(Math.abs(triangle.getScaleY() - anchor.getScaleY()));
-        }
-        if (morph.getScaleX() > anchor.getScaleX()){
-            morph.setScaleX((morph.getScaleX() - anchor.getScaleX()));
-        }
-        if (morph.getScaleY() > anchor.getScaleY()){
-            morph.setScaleY((morph.getScaleY() - anchor.getScaleY()));
-        }
     }
 
 
